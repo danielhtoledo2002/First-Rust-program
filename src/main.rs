1742dcd21ecb5c3e3f726aef689cc1eda2eb8da4
Daniel <0243179@up.edu.mx>
2022-07-06 03:48:17 -0500

    option 2

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use std::io;
 use std::ptr::null;
-
-
 // Importamos try_read que devuelve un error si falla y el tipo de error que devuelve
 use text_io::{try_read, Error};
-
 
 struct Tienda {
     usuario : String,
     total: i32,
-    cantidad: i32,
+    cantidad: Vec<i32>,
     productos:Vec<String>,
 }
 enum Alimentos {
@@ -19,7 +16,7 @@ enum Alimentos {
 }
 
 impl  Tienda {
-    fn new(total: i32) -> Tienda { return Tienda { usuario: "".to_string(), total, cantidad: 0, productos: vec![] } }
+    fn new(total: i32) -> Tienda { return Tienda { usuario: "".to_string(), total, cantidad: vec![0,0,0], productos: vec![] } }
     fn add_value(&mut self, alimento: Alimentos) {
         self.total += match alimento {
             Alimentos::Jamon => {
@@ -44,6 +41,7 @@ fn main() {
     change(&mut s);
     println!("{}", s);
 */
+    let mut cantidad:i32 = 0;
 
 
     let mut usuario:Tienda = Tienda::new(0);
@@ -52,7 +50,6 @@ fn main() {
         menu();
         // Intentamos leer un i32
         let option: Result<i32, Error> = try_read!();
-
         // Si la lectura resultó en error le decimos que no y que vuelva a intentar
         let option = if option.is_err() {
             println!("Error: Ingresa una opción válida!!");
@@ -75,21 +72,31 @@ fn main() {
                     // Si no es error, desenvolvemos el valor en Ok(VALOR)
                     sub_option.unwrap()
                 };
+                println!("{:?}", usuario.cantidad);
                 match sub_option {
                     1 => {
                         usuario.add_value(Alimentos::Jamon);
-
-
-
-
                         usuario.productos.push("Jamón".to_string());
                         usuario.usuario = "Daniel".to_owned();
-                        println!("{},{}, {}, {}",usuario.usuario,usuario.productos[0], usuario.productos.len(), usuario.total);
-
-
+                        usuario.cantidad[0] +=1;
+                        println!("{:?}", usuario.cantidad);
                     }
                     2 =>{
-                        break;
+                        usuario.add_value(Alimentos::Queso);
+                        usuario.productos.push("Carne".to_string());
+                        usuario.usuario = "Daniel".to_owned();
+                        usuario.cantidad[1] +=1;
+
+                        println!("{:?}", usuario.cantidad);
+
+                    }
+                    3 =>{
+                        usuario.add_value(Alimentos::Carne);
+                        usuario.productos.push("Carne".to_string());
+                        usuario.usuario = "Daniel".to_owned();
+                        usuario.cantidad[2] +=1;
+                        println!("{:?}", usuario.cantidad);
+
                     }
                     _ => {}
                 }
@@ -97,7 +104,17 @@ fn main() {
 
 
             }
-            2=>{break}
+            2=>{
+                cantidad = 0;
+                println!("{} Jamón", usuario.cantidad[0]);
+                println!("{} Queso", usuario.cantidad[1]);
+                println!("{} Carne", usuario.cantidad[2]);
+                for i in 0..usuario.cantidad.len(){
+                    println!("{}, {}", usuario.cantidad[i], cantidad);
+                    cantidad += usuario.cantidad[i];
+                }
+                println!("El total de productos es {}, y el preció a pagar es {}", cantidad, usuario.total);
+            }
             _ => {}
         };
     }
